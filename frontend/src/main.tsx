37c4211960197cc71d3b4a0fd080ab8f8d1ce0bf
mzsellem <marleysellem@gmail.com>
2024-03-09 13:39:13 -0500

    add frontend dependencies, add tailwind, create frontend routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Home from "./routes/home";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+   {
+      path: "/",
+      element: <Home />,
+   },
+  //  {
+  //     path: "/patients",
+  //     element: <Patients />,
+  //  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+   <React.StrictMode>
+      <RouterProvider router={router} />
+   </React.StrictMode>
+);
---
+++ frontend/src/routes/home.tsx
@@ -0,0 +1,25 @@
+import Navbar from "../components/navbar";
+import { Worker } from "@react-pdf-viewer/core";
+import { Viewer } from "@react-pdf-viewer/core";
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import { pageNavigationPlugin } from "@react-pdf-viewer/page-navigation";
+import "@react-pdf-viewer/page-navigation/lib/styles/index.css";
+
+// import * as React from "react";
+
+export default function Home() {
+   const pageNavigationPluginInstance = pageNavigationPlugin();
+
+   return (
+      <>
+         <Navbar />
+         <div className="flex text-3xl justify-center p-8">Hello BeyondMD!</div>
+         <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+            <Viewer
+               fileUrl="resume.pdf"
+               plugins={[pageNavigationPluginInstance]}
+            />
+         </Worker>
+      </>
+   );
+}
